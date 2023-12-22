18c6dcf973f9a0ee2761078c1e9735a9be656e32
James <gholamja@cardiff.ac.uk>
2023-12-22 01:07:29 +0000

    optimised sorted projections

--- src/GRDF.jl
+++ src/GRDF.jl
@@ -11,8 +11,8 @@ struct Edge{T, R}
 end
 
 basis(a) = (a ./ norm(a)^2)
-N = 30
-basis_vectors = basis.(reinterpret(SVector{2, Float64}, exp.(Complex.(0, range(0, 2*pi, N)))))
+N = 9
+basis_vectors = basis.(reinterpret(SVector{2, Float64}, exp.(Complex.(0, range(0, pi, N)))))
 res = [zeros(100) for _ in 1:length(basis_vectors)]
 
 # dot(point, basis) = projected point
@@ -21,20 +21,48 @@ proj(a, b::SVector{2, Float64}) = dot(a,b)
 
 const R2 = SVector{2, Float64}
 
-function conv_edges(a::Tuple{R2, R2}, b::Tuple{R2, R2}, r::R2; N = 100, rmax = 1, res = zeros())
-    x = [a[1] ⋅ r, a[2] ⋅ r]
-    y = [b[1] ⋅ r, b[2] ⋅ r]
-    x_left = x[1] < x[2] ? (1, 2) : (2, 1) # get sort indices for dot products
-    y_left = y[1] < y[2] ? (1, 2) : (2, 1) 
-    sx = x[x_left] # create temporary sorted vars
-    sy = y[y_left]
-    sl = sx[1] - sx[2] > sy[1] - sy[2] ? (sy, sx) : (sx, sy) #order from smallest top hat to largest
-    return sl[2][1] - sl[1][2], sl[2][1] - sl[1][2], sl[2][2] - sl[1][1]
+atest = R2((0,0))
+btest = R2((0,1))
+ctest = R2((0,3))
+dtest = R2((0,4))
+
+e1 = (atest, btest)
+e2 = (ctest, dtest)
+bas = basis_vectors[5]
+
+
+fig, ax = plot([e1...])
+plot!(ax, [e2...])
+plot!(ax, basis_vectors[5])
+
+@code_warntype conv_edges(e1, e2, bas)
+@benchmark conv_edges(e1, e2, bas)
+
+function sort2!(x)
+    swap(x) = x[2], x[1]
+    x[1]>x[2] ? swap(x) : x
+end
+
+function conv_edges(a::Tuple{R2, R2}, b::Tuple{R2, R2}, r::R2)
+    x = (a[1] ⋅ r, a[2] ⋅ r)
+    y = (b[1] ⋅ r, b[2] ⋅ r)
+    #print("x vectors are $x, y vectors are $y")
+    sx1, sx2 = sort2!(x)
+    sy1, sy2 = sort2!(y)
+    #order from smallest top hat to largest
+    if sx1 - sx2 > sy1 - sy2
+        return sy1 - sx2, sy1 - sx1, sy2 - sx2, sy2 - sx1
+    else
+        return sx1 - sy2, sx1 - sy1, sx2 - sy2, sx2 - sy1
+    end
 end
 
 function trap2hist(trap, h, rmax, N, res=zeros(N))
     grad = h/(trap[1] - trap[2])
-    l1 = floor(trap[1])
+    ls = floor.(trap)
+    rs = ls .+ 1
+    leftmost = 1
+
 end
 
 
